8b4969997f0cd3059adda33ffb5e4b3d85f6e27d
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-10 00:33:22 -0400

    added pseudocode

---
+++ simple_calculator.py
@@ -0,0 +1,7 @@
+#add a method that allows the user to pick between the operation: addition,subtraction,multiplication,division
+#add a method for "try" function for detecting the error
+#add a method for addition
+#add a method for subtraction
+#add a method for multiplication
+#add a method for division
+#add a font,and colors for the text
