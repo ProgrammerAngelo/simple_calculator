cc60ff7902a10c0ccac42de7ee433633644cf1e8
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-10 00:42:36 -0400

    added a method for adding numbers and finding the sum

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,6 +1,10 @@
 #add a method that allows the user to pick between the operation: addition,subtraction,multiplication,division
 #add a method for "try" function for detecting the error
 #add a method for addition
+number_1 = float(input("Enter first number:\n"))
+number_2 = float(input("Enter second number:\n"))
+sum = number_1 + number_2
+print("The sum is:\n",sum)
 #add a method for subtraction
 #add a method for multiplication
 #add a method for division
