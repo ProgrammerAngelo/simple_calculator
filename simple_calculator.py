f3e9b5fc59e5d1e02bfb710c3ab158e9b6d156df
ProgrammerAngelo <angelovpangilagan@gmail.com>
2024-05-10 00:56:48 -0400

    added a method for deviding numbers and printing the quotient

--- simple_calculator.py
+++ simple_calculator.py
@@ -1,19 +1,27 @@
 #add a method that allows the user to pick between the operation: addition,subtraction,multiplication,division
 #add a method for "try" function for detecting the error
+
 #add a method for addition
 number_1 = float(input("Enter first number:\n"))
 number_2 = float(input("Enter second number:\n"))
 sum = number_1 + number_2
 print("The sum is:\n",sum)
+
 #add a method for subtraction
 number_1 = float(input("Enter first number:\n"))
 number_2 = float(input("Enter second number:\n"))
 difference = number_1 - number_2
 print("The difference is:\n",difference)
+
 #add a method for multiplication
 number_1 =float(input("Enter first number:\n"))
 number_2 = float(input("Enter second number:\n"))
 product = number_1 * number_2
 print("The difference is:\n",product)
+
 #add a method for division
+number_1 =float(input("Enter first number:\n"))
+number_2 = float(input("Enter second number:\n"))
+qoutient = number_1 / number_2
+print("The difference is:\n",qoutient)
 #add a font,and colors for the text
